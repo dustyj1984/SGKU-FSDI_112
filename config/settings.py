eab095806ba51348bca974d2159d2ec1013ecbee
Dustin Jensen <dustyj1984@gmail.com>
2022-11-16 16:17:39 -0800

    Add photo

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 
